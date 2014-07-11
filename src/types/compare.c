9628715784ee681cb2c031ce76261bd0414deeaa
Marc Andrysco <marc.andrysco@gmail.com>
2014-07-11 01:54:58 -0700

    added compare functions
    Added comparison functions for 16-bit and 32-bit unsigned integers.

--- src/types/compare.c
+++ src/types/compare.c
@@ -75,6 +75,46 @@ int compare_uint(const void *p1, const void *p2)
 }
 
 /**
+ * Compares two unsigned 16-bit integers against one another in numerical order.
+ *   @p1: The first integer pointer.
+ *   @p2: The second integer pointer.
+ *   &returns: An integer representing their order.
+ */
+
+_export
+int compare_uint16(const void *p1, const void *p2)
+{
+	uint16_t n1 = *(uint16_t *)p1, n2 = *(uint16_t *)p2;
+
+	if(n1 > n2)
+		return 1;
+	else if(n1 < n2)
+		return -1;
+	else
+		return 0;
+}
+
+/**
+ * Compares two unsigned 32-bit integers against one another in numerical order.
+ *   @p1: The first integer pointer.
+ *   @p2: The second integer pointer.
+ *   &returns: An integer representing their order.
+ */
+
+_export
+int compare_uint32(const void *p1, const void *p2)
+{
+	uint32_t n1 = *(uint32_t *)p1, n2 = *(uint32_t *)p2;
+
+	if(n1 > n2)
+		return 1;
+	else if(n1 < n2)
+		return -1;
+	else
+		return 0;
+}
+
+/**
  * Compares two double-precision floating-point numbers aagainst one another
  * in numerical order.
  *   @p1: The first double pointer.
--- src/types/compare.h
+++ src/types/compare.h
@@ -22,6 +22,8 @@ int compare_str(const void *s1, const void *s2);
 int compare_ptr(const void *p1, const void *p2);
 int compare_int(const void *p1, const void *p2);
 int compare_uint(const void *p1, const void *p2);
+int compare_uint16(const void *p1, const void *p2);
+int compare_uint32(const void *p1, const void *p2);
 int compare_double(const void *p1, const void *p2);
 
 /* %~shim.h% */
